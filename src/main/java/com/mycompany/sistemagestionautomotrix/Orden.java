1660f470017ee06fb2224d4d85f02fe4dc0dd4f9
merly2013 <merlin.landero.ttc.683@unilibre.edu.co>
2026-05-24 18:08:39 -0500

    He creado la clase orden

--- src/main/java/com/mycompany/sistemagestionautomotrix/Orden.java
+++ src/main/java/com/mycompany/sistemagestionautomotrix/Orden.java
@@ -1,6 +1,58 @@
 
 package com.mycompany.sistemagestionautomotrix;
+// @author Merlin landero
+import java.util.ArrayList;
 
 public class Orden {
+    private String id;
+    private String fecha;
+    private String estado;
+    private ArrayList<Servicio> servicios;
+    private Cliente cliente;
+    private Mecanico mecanico;
+    private Vehiculo vehiculo;
 
+   
+    public Orden(String id, String fecha, String estado, Cliente cliente, Mecanico mecanico, Vehiculo vehiculo) {
+        this.id = id;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.cliente = cliente;
+        this.mecanico = mecanico;
+        this.vehiculo = vehiculo;
+        this.servicios = new ArrayList<>();
+    }
+
+    
+    //getter
+    public String getId() {return id;}
+    public String getFecha() {return fecha;}
+    public String getEstado() {return estado;}
+    public Cliente getCliente() {return cliente;}
+    public Mecanico getMecanico() {return mecanico;}
+    public Vehiculo getVehiculo() {return vehiculo;}
+    public ArrayList<Servicio> getServicios() {return servicios;}
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    //metodos
+    public void agregarServicio(Servicio s){
+        
+        if (s != null) {
+            servicios.add(s);
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+
+        for (Servicio s : servicios) {
+            total += s.getCosto();
+        }
+
+        return total;
+    }
 }
+    
