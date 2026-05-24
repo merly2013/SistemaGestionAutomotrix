2dc0f5d4a6456f8598358e5d101a2fa460b28b3a
Senkiu77 <huberdaniel998@gmail.com>
2026-05-23 23:12:46 -0500

    Agregado metodo registrarOrden en Recepcionista

--- src/Recepcionista.java
+++ src/Recepcionista.java
@@ -1,10 +1,10 @@
 package src;
 
-public class Recepsionista extends Persona {
+public class Recepcionista extends Persona {
     
     private String turno;
 
-    public Recepsionista(String nombre, int id, String turno) {
+    public Recepcionista(String nombre, int id, String turno) {
         super(nombre, id);
         this.turno = turno;
     }
@@ -23,6 +23,12 @@ public class Recepsionista extends Persona {
 
     public void setTurno(String turno) {
         this.turno = turno;
+    }
+
+    public void registrarOrden(Orden o){
+        
+        System.out.println("La recepcionista " + nombre + " ha registrado la orden.");
+
     }
 
     
